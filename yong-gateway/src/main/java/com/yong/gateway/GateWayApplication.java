be9f512e1fc577dd46b5543d009f56aff16953b6
LiangYong <yong.a.liang@accenture.com>
2017-12-11 17:10:27 +0800

    update gateway use Oauth2Client ,and pending fixed CorsFilter exception

--- yong-gateway/src/main/java/com/yong/gateway/GateWayApplication.java
+++ yong-gateway/src/main/java/com/yong/gateway/GateWayApplication.java
@@ -1,19 +1,56 @@
 package com.yong.gateway;
 
-import com.yong.resource.config.EnableYongResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableYongResourceServer
-public class GateWayApplication {
+@EnableOAuth2Client
+//@EnableOAuth2Sso
+//TODO try to enable Sso
+@EnableResourceServer
+@ConfigurationProperties(prefix = "yong.unprotected")
+public class GateWayApplication extends ResourceServerConfigurerAdapter{
 
   public static void main(String[] args) {
     SpringApplication.run(GateWayApplication.class, args);
   }
+
+    private List<String> urls = new ArrayList<>();
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/*.html", "/**/*.css", "/**/*.js", "/**/*.png").permitAll()
+            .regexMatchers(urls.stream().toArray(String[]::new)).permitAll()
+            .anyRequest().authenticated().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
 
 }
 
