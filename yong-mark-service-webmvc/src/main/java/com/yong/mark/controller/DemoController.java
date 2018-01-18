51ed025e7d5ce276a6e9f44389181768530536de
LiangYong <yong.a.liang@accenture.com>
2018-01-18 09:57:10 +0800

    add config server test

--- yong-mark-service-webmvc/src/main/java/com/yong/mark/controller/DemoController.java
+++ yong-mark-service-webmvc/src/main/java/com/yong/mark/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.yong.mark.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2017/12/24
  */
 @RestController
+@Slf4j
 public class DemoController {
 
 
@@ -20,6 +24,9 @@ public class DemoController {
 //        return restTemplateBuilder.build();
 //    }
 
+    @Value("${yong.string}")
+    private String testString;
+
     @GetMapping("/hello/{name}")
     private String hello(@PathVariable("name") String name) {
 //        restTemplate.postForEntity("http://localhost:8080/hello",name,null );
@@ -28,7 +35,13 @@ public class DemoController {
     }
 
     @GetMapping("/error")
-    private String error(){
+    private String error() {
         throw new RuntimeException("oh !~!@#!");
+    }
+
+    @GetMapping("/getconfig")
+    private String getConfig() {
+        log.debug("testString = {}", testString);
+        return "testString = " + testString;
     }
 }
