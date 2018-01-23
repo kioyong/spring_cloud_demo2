c0368934e28bca3f74e7eb7e0b770b88e22c5bc7
LiangYong <yong.a.liang@accenture.com>
2018-01-23 14:19:12 +0800

    add stream null point fixed demo

--- yong-mark-service-webmvc/src/test/java/com/yong/mark/StreamTest.java
+++ yong-mark-service-webmvc/src/test/java/com/yong/mark/StreamTest.java
@@ -1,12 +1,14 @@
 package com.yong.mark;
 
-import com.yong.model.Item;
 import com.yong.model.Mark;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static junit.framework.TestCase.*;
 
 /**
  * @acthor yong.a.liang
@@ -16,7 +18,7 @@ import java.util.List;
 public class StreamTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         Mark mark1 = new Mark();
         mark1.setId("1");
         mark1.setActivity(true);
@@ -25,14 +27,52 @@ public class StreamTest {
         mark.setId("2");
         mark.setActivity(false);
         mark.setTitle(null);
+        Mark mark3 = null;
+        String s;
+        List<Mark> marks = Arrays.asList(mark1, mark,mark3);
+//        s = marks.stream().filter(m -> m.isActivity() == false)
+//            .findFirst().map(Mark::getTitle).orElse("");
 
-        List<Mark> marks = Arrays.asList(mark1,mark);
-        String s = marks.stream().filter(m -> m.isActivity() == false)
-            .map(m -> m.getTitle()).findFirst().orElse("123");
-        marks.stream().filter(m -> m.isActivity() == false)
-            .map(m -> m.getTitle()).findFirst().orElse("123").;
-        log.debug("s = {}",s);
+//        Optional<Mark> first = marks.stream().filter(Mark::isActivity).findFirst();
+//        assertFalse(first.isPresent());
 
+//        Optional<String> s1 = marks.stream().filter(m -> !m.isActivity()).findFirst().map(Mark::getTitle);
+//        assertFalse(s1.isPresent());
+
+        s = marks.stream().filter(m -> m.isActivity()).findFirst().map(Mark::getTitle).orElse("nullPointExx");
+        assertEquals(s, "test");
+
+
+    }
+
+    @Test
+    public void test2() {
+
+        Mark mark1 = new Mark();
+        mark1.setId("1");
+        mark1.setActivity(false);
+        mark1.setTitle("test");
+        Mark mark = new Mark();
+        mark.setId("2");
+        mark.setActivity(false);
+        mark.setTitle(null);
+        List<Mark> marks = Arrays.asList(mark1, mark);
+        String s;
+//        s = marks.stream().filter(Mark::isActivity).findFirst().map(Mark::getTitle).orElse("nullPointExx");
+//        assertEquals(s,"test");
+//        mark1.setTitle(null);
+//        s = marks.stream().filter(Mark::isActivity).findFirst().map(Mark::getTitle).orElse("nullPointExx");
+//        assertEquals(s,"nullPointExx");
+//        mark1.setActivity(false);
+
+//        Optional<Mark> first = marks.stream().filter(Mark::isActivity).findFirst();
+//        assertFalse(first.isPresent());
+//
+//        Optional<String> s1 = marks.stream().filter(Mark::isActivity).findFirst().map(Mark::getTitle);
+//        assertFalse(s1.isPresent());
+
+        s = marks.stream().filter(Mark::isActivity).findFirst().map(Mark::getTitle).orElse("nullPointExx");
+        assertEquals(s, "nullPointExx");
     }
 }
 
