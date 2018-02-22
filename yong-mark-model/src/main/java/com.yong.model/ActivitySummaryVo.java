5e71e2e0723206014451ccc9500b1f2a960bbfd2
LiangYong <yong.a.liang@accenture.com>
2018-02-22 14:31:09 +0800

    impl reduce and filter and concatArrays ent aggregation demo

--- yong-mark-model/src/main/java/com.yong.model/ActivitySummaryVo.java
+++ yong-mark-model/src/main/java/com.yong.model/ActivitySummaryVo.java
@@ -2,10 +2,13 @@ package com.yong.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ActivitySummaryVo {
     private String id;
     private Long opener;
     private Long credit;
     private Long fill;
+    private List<ActivitySession> activitySessions;
 }
--- yong-mark-service-webmvc/src/main/java/com/yong/mark/repository/impl/ActivityRepositoryImpl.java
+++ yong-mark-service-webmvc/src/main/java/com/yong/mark/repository/impl/ActivityRepositoryImpl.java
@@ -29,11 +29,24 @@ public class ActivityRepositoryImpl implements ActivityRepositoryCustom {
     @Override
     public List<ActivitySummaryVo> getActivitySummary() {
         Aggregation aggregation = newAggregation(
+//            project()
+//            .and("_id").as("_id")
+//            .andExpression("filter($activitySessions,'activitySession',$$activitySession._id == '1001')").as("activitySessions")
+//            ,
+//            project()
+//                .and("_id").as("id")
+//                .andExpression("reduce(activitySessions,0,add('$$value',sum('$$this.fills.value')))").as("fill")
+//                .andExpression("reduce(activitySessions,0,add('$$value',sum('$$this.credits.value')))").as("credit")
+//                .andExpression("sum(activitySessions.value)").as("opener")
+
             project()
-            .and("_id").as("id")
-            .andExpression("reduce(activitySessions,0,add($$value,sum($$this.fills.value)))").as("fill")
-            .andExpression("reduce(activitySessions,0,add($$value,sum($$this.credits.value)))").as("credit")
-            .andExpression("sum(activitySessions.value)").as("opener")
+                .and("_id").as("id")
+                .andExpression("reduce(activitySessions,new Object[]{},concatArrays('$$value',filter('$$this.fills','fills',$$fills.status=='OPEN' || $$fills.status=='CLOSED')))").as("fills"),
+            project()
+                .and("id").as("id")
+//                .and("activitySessions").as("activitySessions")
+                .andExpression("sum(fills.value)").as("fill")
+
         );
         List<ActivitySummaryVo> result = operations.aggregate(aggregation,"activity",ActivitySummaryVo.class).getMappedResults();
         return result;
