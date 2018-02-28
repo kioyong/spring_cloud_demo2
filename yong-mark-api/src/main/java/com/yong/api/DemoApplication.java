ce3d12e8713561c3933a7132e44a6176fcc4cff9
LiangYong <yong.a.liang@accenture.com>
2018-02-28 19:04:22 +0800

    using 2.0.0M7 version

--- yong-mark-api/src/main/java/com/yong/api/DemoApplication.java
+++ yong-mark-api/src/main/java/com/yong/api/DemoApplication.java
@@ -3,7 +3,7 @@ package com.yong.api;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 
--- yong-mark-api/src/main/java/com/yong/api/client/MarkClient.java
+++ yong-mark-api/src/main/java/com/yong/api/client/MarkClient.java
@@ -1,7 +1,7 @@
 package com.yong.api.client;
 
 import com.yong.model.Mark;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
